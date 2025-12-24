cd249f10845b8e73fb55d7a00aa257d7549abc74
pkaviyarasu16 <kaviyarasu@ops0.com>
2025-12-25 00:32:39 +0530

    Update Terraform configuration

---
+++ terraform/projects/variables.tf
@@ -0,0 +1,35 @@
+variable "aws_region" {
+  description = "AWS region for resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "instance_name" {
+  description = "Name tag for the EC2 instance"
+  type        = string
+  default     = "web-server"
+}
+
+variable "key_pair_name" {
+  description = "Name of the AWS key pair for SSH access"
+  type        = string
+  default     = "my-key-pair"
+}
+
+variable "environment" {
+  description = "Environment name (e.g., dev, staging, prod)"
+  type        = string
+  default     = "dev"
+}
+
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+  default     = "my-project"
+}
+
+variable "ssh_cidr_blocks" {
+  description = "CIDR blocks allowed for SSH access"
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+}
